33953545e3a1e85865f5ec960c059422113ab231
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-01 19:41:38 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,18 @@
 # Add  code here!
-def prime?(number)
-    if number <= 1
-        return false
-    else
-        range = (2..number-1).to_a
-        if range.any? { |n| number % n == 0 }
+require 'benchmark'
+
+Benchmark.bmbm(7) do |bm|
+  bm.report('reverse') do
+    def prime?(number)
+        if number <= 1
             return false
         else
-            return true
+            range = (2..number-1).to_a
+            if range.any? { |n| number % n == 0 }
+                return false
+            else
+                return true
+            end
         end
     end
 end
