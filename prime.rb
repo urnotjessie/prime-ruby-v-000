3475720b9337d43662f4667f439245b55de83b12
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-01 19:36:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,8 @@
 def prime?(number)
     if number < 0
         return false
+    elsif number == 1 || number == 2
+        return true
     else
         range = (2..number-1).to_a
         if range.any? { |n| number % n == 0 }
