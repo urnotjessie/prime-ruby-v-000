1728ec0effd32094005c30b780256d074005951f
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-01 19:43:22 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 require 'benchmark'
 
 Benchmark.bmbm(7) do |bm|
-  bm.report('reverse') do
+  bm.report('jessie') do
     def prime?(number)
         if number <= 1
             return false
@@ -16,4 +16,21 @@ Benchmark.bmbm(7) do |bm|
         end
     end
 end
+
+bm.report('solution') do 
+    def prime?(num)
+  if num < 0 or num == 0 or num == 1
+    return false
+  else
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
+    end
+  end
 end
+
+end
+
+
+end
+
+
